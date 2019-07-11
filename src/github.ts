74aa7b69d91f79daf84e4ad088aaa0024d6472b5
François Hodierne <francois@hodierne.net>
2019-07-11 16:03:10 +0200

    feat: createFunding

--- src/github.ts
+++ src/github.ts
@@ -132,3 +132,56 @@ export async function removeLabelsFromGithubIssue(
 
   return labels
 }
+
+/**
+ *
+ * Create or Re-Create branch on a given repository
+ *
+ * @param github
+ * @param owner
+ * @param repo
+ * @param branchName
+ */
+export async function resetBranch(
+  github: Octokit,
+  owner: string,
+  repo: string,
+  branchName: string,
+  defaultBranchName: string,
+) {
+  // Get if the BRANCH_NAME is already existing
+  const githubBranch = await github.repos
+    .getBranch({
+      owner,
+      repo,
+      branch: branchName,
+    })
+    .then((res: any) => res.data)
+    .catch(() => null)
+
+  // Delete the branch in this case
+  if (githubBranch) {
+    await github.git.deleteRef({
+      owner,
+      repo,
+      ref: `heads/${branchName}`,
+    })
+  }
+
+  // Get the reference for the default branch (not necessarily master)
+  const reference = await github.git
+    .getRef({
+      owner,
+      repo,
+      ref: `heads/${defaultBranchName}`,
+    })
+    .then((res: any) => res.data)
+
+  // Create the target branch
+  return await github.git.createRef({
+    owner,
+    repo,
+    ref: `refs/heads/${branchName}`,
+    sha: reference.object.sha,
+  })
+}
