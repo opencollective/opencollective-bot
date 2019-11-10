4a6124c4fa35a84a4edc04e90cae56df681613de
Caner <ccanersezgin@gmail.com>
2019-11-10 22:41:46 +0300

    main functions of adding funding to package.json

---
+++ src/actions/addPackageJsonFunding.ts
@@ -0,0 +1,119 @@
+import mls from 'multilines'
+import Octokit, { ReposGetResponse } from '@octokit/rest'
+
+import { base64 } from '../utils'
+import { getCollectiveWithGithubHandle } from '../collective'
+import { resetBranch } from '../github'
+
+import defaultFundingPackageAsString from '../assets/default-funding-package'
+
+const BRANCH_NAME = `opencollective-bot/funding_package`
+
+const FILE_PATH = 'package.json'
+
+const PR_TITLE = 'Add FUNDING to package.json'
+
+const PR_BODY = mls`
+  | Add FUNDING to package.json
+  |
+  | Add FUNDING to package.json
+  |
+  | Add FUNDING to package.json
+  `
+
+const FUNDING_DEFAULT_FILE_CONTENT = defaultFundingPackageAsString
+
+function updatePackageJson(packageJSON: String, newContent: String) {
+  const removeLastChar = packageJSON.substr(0, packageJSON.lastIndexOf('}'))
+  const indexes = [
+    removeLastChar.lastIndexOf('"'),
+    removeLastChar.lastIndexOf(']'),
+    removeLastChar.lastIndexOf('}'),
+  ]
+  const index = Math.max(...indexes)
+  return (
+    removeLastChar.substr(0, index + 1) +
+    `,
+  ${newContent}
+}`
+  )
+}
+
+export default async function addPackageJsonFunding(
+  github: Octokit,
+  repoResponse: ReposGetResponse,
+) {
+  const owner = repoResponse.owner.login
+  const repo = repoResponse.name
+
+  // Check if package.json file is existing
+  const packageJsonFile = await github.repos
+    .getContents({
+      owner,
+      repo,
+      path: 'package.json',
+    })
+    .then((res: any) => res.data)
+    .catch(() => null)
+
+  if (!packageJsonFile) {
+    console.log(`There is not package.json on ${owner}/${repo}`)
+    return
+  }
+  const packageJsonContent = Buffer.from(
+    packageJsonFile.content,
+    'base64',
+  ).toString()
+  if (packageJsonContent.includes('"funding":')) {
+    console.log(`Funding property already exists on ${owner}/${repo}`)
+    return
+  }
+
+  let fileContent = FUNDING_DEFAULT_FILE_CONTENT
+
+  // Fetch the collective from Open Collective
+  const collective = await getCollectiveWithGithubHandle(`${owner}/${repo}`)
+
+  // Replace placeholder with proper collective slug
+  if (collective) {
+    fileContent = fileContent.replace('<YOUR-COLLECTIVE-SLUG>', collective.slug)
+  }
+
+  // Get information about the repo
+  // we're only interested in the default_branch
+  const { default_branch: defaultBranchName } = await github.repos
+    .get({ owner, repo })
+    .then((res: any) => res.data)
+
+  await resetBranch(github, owner, repo, BRANCH_NAME, defaultBranchName)
+
+  const updatedPackageJsonContent = updatePackageJson(
+    packageJsonContent,
+    fileContent,
+  )
+  // Update Package.json
+  await github.repos.createOrUpdateFile({
+    owner,
+    repo,
+    path: FILE_PATH,
+    message: `Funding added to ${FILE_PATH}`,
+    content: base64(updatedPackageJsonContent),
+    sha: packageJsonFile.sha,
+    branch: BRANCH_NAME,
+  })
+
+  // Create the PR
+  const pr = await github.pulls
+    .create({
+      owner,
+      repo,
+      head: BRANCH_NAME,
+      base: defaultBranchName,
+      title: PR_TITLE,
+      body: PR_BODY,
+      maintainer_can_modify: true,
+    })
+    .then((res: any) => res.data)
+
+  console.log(`PR created for adding fund property to package.json: ${pr.url}`)
+}
---
+++ src/assets/default-funding-package.ts
@@ -0,0 +1,6 @@
+const defaultFundingPackage = `"funding": {
+    "type": "opencollective",
+    "url": "https://opencollective.com/<YOUR-COLLECTIVE-SLUG>"
+  }`
+
+export default defaultFundingPackage
