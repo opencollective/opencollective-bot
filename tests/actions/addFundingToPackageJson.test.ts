e8df66b1ebba5e7bfdbee971cb6f3b6866d20c35
Caner <ccanersezgin@gmail.com>
2019-11-19 00:29:54 +0300

    next line added && private property check added

--- tests/actions/addFundingToPackageJson.test.ts
+++ tests/actions/addFundingToPackageJson.test.ts
@@ -339,6 +339,86 @@ describe('opencollective installation_repositories', () => {
     expect(github.repos.createOrUpdateFile).toBeCalledTimes(0)
     expect(github.pulls.create).toBeCalledTimes(0)
   })
+
+  test('do nothing, if package.json contains private: true', async () => {
+    nock('https://api.opencollective.com/')
+      .post('/graphql/v2')
+      .reply(200, {
+        data: {
+          collective: {
+            slug: 'backyourstack',
+          },
+        },
+      })
+
+    const app = new probot.Application()
+
+    const github = {
+      repos: {
+        get: jest.fn().mockResolvedValue({
+          data: repository,
+        }),
+        getBranch: jest.fn().mockRejectedValue(new Error('Branch not found')),
+        getContents: jest.fn().mockImplementation(() => {
+          return Promise.resolve({
+            data: {
+              name: 'package.json',
+              path: 'package.json',
+              /*  
+                    content: 
+                    {
+                        "name": "backyourstack",
+                        "version": "1.1.0",
+                        "funding": {
+                            "type": "opencollective",
+                            "url": "https://opencollective.com/backyourstack"
+                        }
+                    }
+                    */
+              content:
+                'ewogICAgICAibmFtZSI6ICJiYWNreW91cnN0YWNrIiwKICAgICAgInZlcnNp\nb24iOiAiMS4xLjAiLAogICAgICAiamVzdCI6IHsKICAgICAgICAgICAgImNv\ndmVyYWdlRGlyZWN0b3J5IjogIi4vY292ZXJhZ2UvIiwKICAgICAgICAgICAg\nImNvbGxlY3RDb3ZlcmFnZSI6IHRydWUKICAgICAgfSwKICAgICAgInByaXZh\ndGUiOiB0cnVlCn0K\n',
+            },
+          })
+        }),
+        createOrUpdateFile: jest.fn(),
+      },
+      git: {
+        getRef: jest.fn().mockResolvedValue({
+          // we're just interested in sha
+          data: {
+            object: { sha: 'a9993e364706816aba3e25717850c26c9cd0d89d' },
+          },
+        }),
+        createRef: jest.fn(),
+        deleteRef: jest.fn(),
+      },
+      pulls: {
+        create: jest.fn().mockResolvedValue({
+          // we're just interested in url
+          data: { url: 'https://github.com/znarf/backyourstack/pull/11' },
+        }),
+      },
+    }
+
+    // Mock out GitHub client
+    app.load(opencollective)
+    app.auth = () => Promise.resolve(github as any)
+
+    await app.receive({
+      id: '123',
+      name: 'installation_repositories',
+      payload: installationRepositoriesFixture,
+    })
+
+    expect(github.repos.getContents).toBeCalledTimes(1)
+    expect(github.repos.get).toBeCalledTimes(1)
+    expect(github.repos.getBranch).toBeCalledTimes(0)
+    expect(github.git.deleteRef).toBeCalledTimes(0)
+    expect(github.git.getRef).toBeCalledTimes(0)
+    expect(github.git.createRef).toBeCalledTimes(0)
+    expect(github.repos.createOrUpdateFile).toBeCalledTimes(0)
+    expect(github.pulls.create).toBeCalledTimes(0)
+  })
 })
 
 describe('package.json indents', () => {
